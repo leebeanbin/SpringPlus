5825f226f6d30264ac7087bd3b8199039b033d41
leebeanbin <wjdqlsdy1004@naver.com>
2024-07-05 17:02:46 +0900

    add : add more instance for signup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 INSERT INTO company(id,created_at,updated_at,domain,name,status)
 VALUES(1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'gmail.com','이튜','ACTIVE');
 
+INSERT INTO company(id,created_at,updated_at,domain,name,status)
+VALUES(2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'naver.com','NAVER','ACTIVE');
+
 
